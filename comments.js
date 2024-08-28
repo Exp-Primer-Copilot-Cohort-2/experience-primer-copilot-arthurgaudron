d0de4057aaabec67fb9d92862b6436f3aa5fcc0b
arthurgaudron <arthur@gaudron.org>
2024-08-28 13:00:27 +0000

    Copilot third commit other suggestion

--- comments.js
+++ comments.js
@@ -1,12 +1,56 @@
 // create web server
-const express = require('express');
-const app = express();
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var comments = require('./comments');
+var mime = require('mime');
+var url = require('url');
 
-// create a route
-app.get('/comments', (req, res) => {
-  res.send('This is the comments page.');
+var server = http.createServer(function(request, response) {
+  var urlPath = url.parse(request.url).pathname;
+  var filePath = path.join(__dirname, urlPath);
+  console.log(filePath);
+  if (urlPath === '/comments.json') {
+    comments.get(function(err, data) {
+      if (err) {
+        response.writeHead(500, {
+          'Content-Type': 'text/plain'
+        });
+        response.end(err);
+        return;
+      }
+      response.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      });
+      response.end(data);
+    });
+  } else {
+    fs.exists(filePath, function(exists) {
+      if (exists) {
+        fs.readFile(filePath, function(err, data) {
+          if (err) {
+            response.writeHead(500, {
+              'Content-Type': 'text/plain'
+            });
+            response.end(err);
+            return;
+          }
+          response.writeHead(200, {
+            'Content-Type': mime.lookup(filePath)
+          });
+          response.end(data);
+        });
+      } else {
+        response.writeHead(404, {
+          'Content-Type': 'text/plain'
+        });
+        response.end('404 Not Found\n');
+      }
+    });
+  }
 });
 
-app.listen(3000, () => {
+server.listen(3000, function() {
   console.log('Server is listening on port 3000');
 });
